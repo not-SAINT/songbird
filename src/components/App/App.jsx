d06ddd3db1d7739874efe9146e67dff605611d20
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-07-31 16:51:47 +0300

    feat: add app structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import style from './App.module.scss';
+
+const App = () => {
+  return <div className={style.App__wrapper}>Start</div>;
+};
+
+export default App;
---
+++ src/constants/common.js
@@ -0,0 +1,2 @@
+export const MAX_REWARD = 5;
+export const PENALTY = 1;
